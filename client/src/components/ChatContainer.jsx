816fb7103214032d9542f050835e6723c75f9dcf
Himanshu Verma <himanshuverma4765@gmail.com>
2026-08-26 15:25:01 +0530

    feat: update logo assets and SVG graphics

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -71,7 +71,6 @@ const ChatContainer = () => {
           alt=""
           className="md:hidden max-w-7"
         />
-        <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
       </div>
 
       {/* Chat Part */}
